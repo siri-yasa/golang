36108ec6eaccc8b4ee3b785f73107e0f619ee4f9
siri-yasa <111823423+siri-yasa@users.noreply.github.com>
2022-09-18 17:53:30 -0500

    Update posts.go

--- posts.go
+++ posts.go
@@ -5,14 +5,7 @@ import (
 	"fmt"
 
 	_ "github.com/go-sql-driver/mysql"
-)
-
-/*import (
-	"database/sql"
-	"fmt"
-
-	_ "github.com/go-sql-driver/mysql"
-)*/
+)	
 
 type Post struct {
 	Id   int
@@ -21,72 +14,45 @@ type Post struct {
 }
 
 func main() {
-	db, e := sql.Open("mysql", "root:Temp-Pass-27@/recordings")
-
-	ErrorCheck(e) // ("mysql", " <user>:<password>@/<databasename>" )
-
-	// INSERT INTO DB
-	// prepare
-
-	fmt.Println("siri", db)
+	db, e := sql.Open("mysql", "root:Temp-Pass-27@/recordings")  
+	ErrorCheck(e)                                                                        // ("mysql", " <user>:<password>@/<databasename>" )
 	stmt, e := db.Prepare("insert into posts(id, Name, Text) values (?, ?, ?)")
 	ErrorCheck(e)
-
-	//execute
 	res, e := stmt.Exec("5", "Post five", "Contents of post 5")
 	ErrorCheck(e)
-
 	id, e := res.LastInsertId()
 	ErrorCheck(e)
-
-	fmt.Println("Insert id", id)
-
-	//Update db
+	fmt.Println("Insert id", id)	
 	stmt, e = db.Prepare("update posts set Text=? where id=?")
 	ErrorCheck(e)
-
-	// execute
 	res, e = stmt.Exec("This is post five", "5")
 	ErrorCheck(e)
 	a, e := res.RowsAffected()
 	ErrorCheck(e)
-
 	fmt.Println(a)
-
-	// query all data
 	rows, e := db.Query("select * from posts")
 	ErrorCheck(e)
-
 	var post = Post{}
-
 	for rows.Next() {
 		e = rows.Scan(&post.Id, &post.Name, &post.Text)
 		ErrorCheck(e)
 		fmt.Println(post)
 	}
-	// delete data
 	stmt, e = db.Prepare("delete from posts where id=?")
 	ErrorCheck(e)
-
-	// delete 5th post
 	res, e = stmt.Exec("5")
 	ErrorCheck(e)
-
-	// affected rows
 	a, e = res.RowsAffected()
 	ErrorCheck(e)
-
 	fmt.Println(a) // 1
-	// close database after all work is done
 	defer db.Close()
-}
-func ErrorCheck(err error) {
+      }
+        func ErrorCheck(err error) {
 	if err != nil {
 		panic(err.Error())
 	}
-}
-
-func PingDB(db *sql.DB) {
+     }
+     func PingDB(db *sql.DB) {
 	err := db.Ping()
 	ErrorCheck(err)
-}
+     }
